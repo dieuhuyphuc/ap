137cee12f4f188dfce5f3f05ed72b17b36cc10d7
dieuhuyphuc <dieuhuyphuc@gmail.com>
2021-06-18 15:15:03 +0700

    update

---
+++ ex2/ex/ex2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace ex2.ex2
+{
+    public class ex2
+    {
+        public static void Main(string[] args)
+        {
+            SearchWord();
+        }
+
+        private static void SearchWord()
+        {
+            string str;
+            string wordS;
+            int count = 0;
+            Console.WriteLine("Enter a String");
+            str = Console.ReadLine();
+            Console.WriteLine("Enter a word to search");
+            wordS = Console.ReadLine();
+            string[] c = str.Split();
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (wordS != null && wordS.Equals(c[i]))
+                {
+                    count++;
+                }
+                
+            }
+
+            if (count <= 1 )
+            {
+                Console.WriteLine($"Word found {count} time in the string");
+            }
+            else
+            {
+                Console.WriteLine($"Word found {count} times in the string");
+            }
+        }
+    }
+}
--- ex2/ex2/ex2.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ex2.ex2
-{
-    public class ex2
-    {
-        
-    }
-}
